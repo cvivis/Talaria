068c7c1d8c482bafc84a1edbf3ece3c37919a733
Lee-Jiseung <ljss98@naver.com>
2023-10-31 11:02:08 +0900

    ✨ Feat: Implement swagger-editor, swagger-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import SwaggerEditor from 'swagger-editor';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-editor/dist/swagger-editor.css'
+import 'swagger-ui-react/swagger-ui.css';
 
 function App() {
+  useEffect(() => {
+    console.log('hello')
+    // Swagger Editor 초기화
+    const editor = SwaggerEditor({
+      dom_id: '#swagger-editor',
+      layout: 'EditorLayout',
+      // plugins: Object.values(plugins),
+      swagger2GeneratorUrl: 'https://generator.swagger.io/api/swagger.json',
+      oas3GeneratorUrl: 'https://generator3.swagger.io/openapi.json',
+      swagger2ConverterUrl: 'https://converter.swagger.io/api/convert',
+    });
+
+    return () => {
+      // 컴포넌트 언마운트 시 Swagger Editor 해제
+      if(editor) {
+        editor.destroy();
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>API Documentation</h1>
+
+      {/* Swagger Editor */}
+      <div id="swagger-editor"></div>
+
+      <hr />
+      <br></br>
+
+      <h2>API Specification</h2>
+      {/* Swagger UI */}
+      <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
     </div>
   );
 }
