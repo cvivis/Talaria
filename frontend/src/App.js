1f2a141cf1400da2fc828581424f65c924e8dc6f
오승기 <dhtmdrl2004@knou.ac.kr>
2023-10-31 16:44:15 +0900

    :package: Package: Add Router & ChakraUI Package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,22 @@
-import React, { useEffect } from 'react';
-import SwaggerEditor from 'swagger-editor';
 import SwaggerUI from 'swagger-ui-react';
-import 'swagger-editor/dist/swagger-editor.css'
-import 'swagger-ui-react/swagger-ui.css';
+import "swagger-ui-react/swagger-ui.css";
+import SwaggerTest from '../src/SwaggerTest.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './Main';
 
 function App() {
-  useEffect(() => {
-    console.log('hello')
-    // Swagger Editor 초기화
-    const editor = SwaggerEditor({
-      dom_id: '#swagger-editor',
-      layout: 'EditorLayout',
-      // plugins: Object.values(plugins),
-      swagger2GeneratorUrl: 'https://generator.swagger.io/api/swagger.json',
-      oas3GeneratorUrl: 'https://generator3.swagger.io/openapi.json',
-      swagger2ConverterUrl: 'https://converter.swagger.io/api/convert',
-    });
-
-    return () => {
-      // 컴포넌트 언마운트 시 Swagger Editor 해제
-      if(editor) {
-        editor.destroy();
-      }
-    };
-  }, []);
 
   return (
-    <div>
-      <h1>API Documentation</h1>
-
-      {/* Swagger Editor */}
-      <div id="swagger-editor"></div>
-
-      <hr />
-      <br></br>
-
-      <h2>API Specification</h2>
-      {/* Swagger UI */}
-      <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+					<Route path="/" element={<Main />} />
+          <Route path="/editor" element={<SwaggerTest />} />
+          <Route path="/ui" element={<SwaggerUI url='https://petstore3.swagger.io/api/v3/openapi.json' />} />
+					{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+					{/* <Route path="*" element={<NotFound />}></Route> */}
+				</Routes>
+      </BrowserRouter>
     </div>
   );
 }
