e79c4da6366be44443f458c56cc1f7dbd6d54f9d
오승기 <dhtmdrl2004@knou.ac.kr>
2023-11-01 17:20:06 +0900

    :sparkles: Feat: Create Public/Private Route

---
+++ frontend/src/components/route/PrivateRoute.js
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({selectRole, component : Component}) => {
+    const login = true;
+    const role = "user"; 
+
+    if(!login) {
+        return <Navigate to="/" />
+    }
+
+    if(role !== selectRole) {
+        alert("권한이 없는 계정 입니다.");
+        return <Navigate to="/" />
+    }
+
+    return Component;
+}
+
+export default PrivateRoute;
