4fd67f1b3d0d72028f7035bfcdd30004d9342a0d
이승민 <0283esm@naver.com>
2023-11-03 12:26:29 +0900

    :tada: Init: Init monitoring project

---
+++ monitoring/src/main/java/com/hermes/monitoring/config/HttpStatusConfig.java
@@ -0,0 +1,82 @@
+package com.hermes.monitoring.config;
+
+
+import com.hermes.monitoring.dto.LogDto;
+import com.hermes.monitoring.dto.MonitorDto;
+import com.hermes.monitoring.parser.LogParser;
+import com.hermes.monitoring.service.LogService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class HttpStatusConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private LogParser logParser = new LogParser();
+    private final LogService logService;
+    private List<LogDto> logDataList;
+    private MonitorDto monitorDto;
+
+
+    @Bean
+    public Job HttpStatusJob(){
+        System.out.println("시작");
+        return jobBuilderFactory.get("httpStatus")
+                .incrementer(new RunIdIncrementer())
+                .start(flatFileItemReaderStep())
+                .next(checkConnectionCountStep())//
+                .next(sendLogFileToFrontStep()) // 프론트 단에 보내주기
+                .build();
+    }
+
+    // 로그 파일을 불러오고 5초동안 요청한 로그 DTO들을 가져오는 스탭
+    @Bean
+    public Step flatFileItemReaderStep() {
+        return stepBuilderFactory.get("flatFileItemReader")
+                .tasklet((contribution, chunkContext) ->{
+                    logDataList = logParser.parseLog("C:/Users/SSAFY/Desktop/Project/S09P31A107/monitoring/log.txt");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    // 모니터링에 보여줄 모니터링DTO를 변환해주는 스탭
+    @Bean
+    public Step checkConnectionCountStep() {
+        return stepBuilderFactory.get("checkConnection")
+                .tasklet((contribution, chunkContext) -> {
+                    monitorDto = new MonitorDto(new Date(), logDataList.size());
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    // 웹 소켓으로 모니터링 정보를 보내주는 스탭
+    @Bean
+    public Step sendLogFileToFrontStep() {
+        return stepBuilderFactory.get("sendLogFileFront")
+                .tasklet((contribution, chunkContext) -> {
+                    logService.sendMonitorDtoToClient("/sub/log",monitorDto);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
+
+
+}
