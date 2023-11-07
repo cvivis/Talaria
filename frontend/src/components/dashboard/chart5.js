f855acd089eb3e47a0dac8e3f9b6641eaddc5366
cvivis <hsy0754@naver.com>
2023-11-08 00:59:02 +0900

    :pencil2: Fix: multi graph add

--- frontend/src/components/dashboard/chart5.js
+++ frontend/src/components/dashboard/chart5.js
@@ -1,72 +1,99 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Box,Text } from "@chakra-ui/react";
-import * as StompJs from '@stomp/stompjs';
-import {Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,TableContainer,} from '@chakra-ui/react'
+import React, { useEffect, useRef, useState } from "react";
+import { Box, Center, Text } from "@chakra-ui/react";
+import * as StompJs from "@stomp/stompjs";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 function Chart5() {
-    const client = useRef({}); 
-    const [data,setData] = useState([]);
-    const connect = () =>{
-        client.current = new StompJs.Client({
-            brokerURL: 'ws://localhost:8080/ws/monitoring',
-            onConnect:() =>{
-               // Do something, all subscribes must be done is this callback
-              console.log("연결 SUB");
-                subscribe();
-            },
-        });
-        client.current.activate();
+  const client = useRef({});
+  const [data, setData] = useState([]);
+  const connect = () => {
+    client.current = new StompJs.Client({
+      brokerURL: "ws://localhost:8080/ws/monitoring",
+      onConnect: () => {
+        // Do something, all subscribes must be done is this callback
+        console.log("연결 SUB");
+        subscribe();
+      },
+    });
+    client.current.activate();
+  };
+
+  const disconnect = () => {
+    client.current.deactivate(); // 활성화된 연결 끊기
+  };
+
+  const subscribe = () => {
+    client.current.subscribe("/sub/error-count", (res) => {
+      // server에게 메세지 받으면
+      const json_body = JSON.parse(res.body);
+      setData(json_body.slice());
+      console.log(data.map((info, rank) => info.errorType));
+    });
+  };
+
+  const getTextColor = (type) => {
+    if (type === "EMERG") {
+      return "Blue";
+    } else if (type === "CRIT") {
+      return "Green";
+    } else if (type === "ERROR") {
+      return "Red";
+    } else {
+      return "Orange";
     }
-  
-    const disconnect = () => {
-        client.current.deactivate(); // 활성화된 연결 끊기 
-    };
-  
-    const subscribe = () => {
-      client.current.subscribe('/sub/error-count', (res) => { // server에게 메세지 받으면
-        const json_body = JSON.parse(res.body);
-        setData(json_body.slice());
-        console.log(data.map((info,rank)=>info.errorType));
-        });
-    };
+  };
 
-    useEffect(() => {
-        connect();
-        // const interval = setInterval(() => {
-            
-        // }, 1000);
-    
-        return () => {
-    
-        //   clearInterval(interval);
-          disconnect();
-        };
-      }, []);
+  useEffect(() => {
+    connect();
+    // const interval = setInterval(() => {
 
-    return (
-        <>
-        <Box bg='white' w='36vw' h='55vh' borderRadius="20px"  boxShadow="lg">
-            <Text fontWeight='Bold' p={5} >Error Count</Text>
-            <TableContainer>
-            <Table variant='simple' w="100%" h="100%" minH="50px">
-                <Thead>
-                <Tr>
-                    <Th>Error Type</Th>
-                    <Th isNumeric>Count</Th>
+    // }, 1000);
+
+    return () => {
+      //   clearInterval(interval);
+      disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <Box bg="white" w="36vw" h="55vh" borderRadius="20px" boxShadow="lg">
+        <Text fontWeight="Bold" p={5}>
+          Error Count
+        </Text>
+        <TableContainer>
+          <Table variant="simple" w="100%" h="100%" minH="50px">
+            <Thead>
+              <Tr>
+                <Th>Error Type</Th>
+                <Th isNumeric>Count</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {data.map((info, index) => (
+                <Tr key={index}>
+                  <Td color={getTextColor(info.errorType)} fontWeight="Bold" border="none">
+                    {info.errorType}
+                  </Td>
+                  <Td isNumeric style={{ paddingRight: "13%" }} border="none">
+                    <div style={{ margin: "15% auto" }}>{info.count}</div>
+                  </Td>
                 </Tr>
-                </Thead>
-                <Tbody>
-                    {data.map((info,index)=>(
-                    <Tr key={index}>
-                        <Td>{info.errorType}</Td>
-                        <Td isNumeric>{info.count}</Td>
-                        {/* <Td>{info.detailInfo}</Td> */}
-                    </Tr>
-                    ))} 
-                </Tbody>
-            </Table>
-            </TableContainer>
-        </Box>
-        </>
-    );
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
 }
 export default Chart5;
--- frontend/src/components/dashboard/chart6.js
+++ frontend/src/components/dashboard/chart6.js
@@ -1,179 +1,171 @@
-import { useEffect, useState,useRef} from "react";
-import { Box,Text } from "@chakra-ui/react";
-import * as StompJs from '@stomp/stompjs';
-import ReactApexChart from 'react-apexcharts';
-import ApexCharts from 'apexcharts';
+import { useEffect, useState, useRef } from "react";
+import { Box, Text } from "@chakra-ui/react";
+import * as StompJs from "@stomp/stompjs";
+import ReactApexChart from "react-apexcharts";
+import ApexCharts from "apexcharts";
 function Chart6() {
+  var data = [];
 
-    var data = []
+  const [successCount, setSuccessCount] = useState([]);
+  const [failCount, setFailCount] = useState([]);
+  const [serverFailCount, setServerFailCount] = useState([]);
+  // let XAXISRANGE = 1800000;
+  const [XAXISRANGE, SetXAXISRANGE] = useState(60000);
+  const [options, setOptions] = useState({
+    chart: {
+      id: "realtime", // ApexCharts의 메서드 호출 시 필요 (brush chart, syncronized chart, calling exec)
+      height: 350, // 그래프 높이
+      type: "line", // 차트 타입 (line , donut,treemap ... )
+      animations: {
+        enabled: true,
+        easing: "linear", // 애니메이션 속도 변화률 linear = 등속
+        dynamicAnimation: {
+          enabled: true, // 데이터 바뀔 때 re-rendering
+          speed: 1000, // 데이터 바뀔 때 run 속도
+        },
+      },
+      toolbar: {
+        //다운로드같은 메뉴바
+        show: false,
+      },
+      zoom: {
+        enabled: false, // 확대 이동 가능 <- realchart에서는 false
+      },
+    },
+    // colors: ["blue", "orange", "red"],
+    dataLabels: {
+      enabled: false, //데이터에 라벨링 <- realchart에서는 false
+    },
+    stroke: {
+      curve: "smooth", // 데이터 꺾는 정도
+    },
+    markers: {
+      size: 0, // 수정 X
+      // colors: ["blue", "orange", "red"],
+    },
+    xaxis: {
+      type: "datetime", // category, datetime, numeric
+      range: XAXISRANGE, //최대, 최소 값을 동적으로 받기위한 용도 ?
+    },
+    yaxis: {
+      max: 100,
+      min: 0,
+    },
+    legend: {
+      show: false, //??
+    },
+  });
 
-    const [successCount, setSuccessCount] = useState(0);
-    const [failCount, setfailCount] = useState(0);
-    const [serverFailCount, setserverFailCount] = useState(0);
-    // let XAXISRANGE = 1800000;
-    const [XAXISRANGE, SetXAXISRANGE] = useState(600000);
-    const [options, setOptions] = useState({
-      chart: {
-        id: 'realtime', // ApexCharts의 메서드 호출 시 필요 (brush chart, syncronized chart, calling exec)
-        height: 350, // 그래프 높이 
-        type: 'line', // 차트 타입 (line , donut,treemap ... )
-        animations: {
-          enabled: true,
-          easing: 'linear', // 애니메이션 속도 변화률 linear = 등속 
-          dynamicAnimation: { 
-            enabled : true, // 데이터 바뀔 때 re-rendering
-            speed: 1000  // 데이터 바뀔 때 run 속도
-          }
-        },
-        toolbar: { //다운로드같은 메뉴바 
-          show: false,
-        },
-        zoom: {
-          enabled: false // 확대 이동 가능 <- realchart에서는 false
-        }
-      },
-      dataLabels: {
-        enabled: false //데이터에 라벨링 <- realchart에서는 false
-      },
-      stroke: {
-        curve: 'smooth' // 데이터 꺾는 정도
-      },
-      markers: {
-        size: 0 // 수정 X
-      },
-      xaxis: {
-        type: 'datetime', // category, datetime, numeric
-        range: XAXISRANGE //최대, 최소 값을 동적으로 받기위한 용도 ?
-      },
-      yaxis: {
-        max: 10,
-        min: 0
-      },
-      legend: {
-        show: false //?? 
+  const client = useRef({});
+  const connect = () => {
+    client.current = new StompJs.Client({
+      brokerURL: "ws://localhost:8080/ws/monitoring",
+      onConnect: () => {
+        // Do something, all subscribes must be done is this callback
+        console.log("연결 SUB");
+        subscribeToTopic("/sub/success-time-check", subscribeSuccess);
+        subscribeToTopic("/sub/fail-time-check", subscribeFail);
+        subscribeToTopic("/sub/server-fail-time-check", subscribeServerFail);
       },
     });
-    
-    function getNewSeries() {
-      
-      data.push({
-        x: new Date(),
-        // y: Math.floor(Math.random() * (10 -  0 + 1)) + 0
-        y:10,
-      })
-      console.log(data)
-    }
-    
-    const [series, setSeries] = useState([
-      {
-        data: data.slice() // 배열 복사 
-      }
-    ]);
-    
-    // const ranking = useRef([]);
-    const [ranking, setranking] = useState([]);
-    // const [errorCount, setErrorCount] = useState([]);
-    useEffect(() => {
-      console.log("targets",ranking);
-    }, [ranking])
-    
-    const client = useRef({}); 
-    const [log,setLog] = useState("");
-    const connect = () =>{
-        client.current = new StompJs.Client({
-            brokerURL: 'ws://localhost:8080/ws/monitoring',
-            onConnect:() =>{
-               // Do something, all subscribes must be done is this callback
-              console.log("연결 SUB");
-                subscribeToTopic('/sub/success-time-check', subscribeSuccess);
-            },
-        });
-        client.current.activate();
-    }
-  
-    const disconnect = () => {
-      client.current.deactivate(); // 활성화된 연결 끊기 
+    client.current.activate();
   };
-  
-  const subscribeToTopic = (topic,callback) => {
+
+  const disconnect = () => {
+    client.current.deactivate(); // 활성화된 연결 끊기
+  };
+
+  useEffect(() => {
+    setSeries((prevSeries) => [
+      {
+        name: "success",
+        data: successCount.slice(),
+      },
+      {
+        name: "fail",
+        data: failCount.slice(),
+      },
+      {
+        name: "serverFail",
+        data: serverFailCount.slice(),
+      },
+    ]);
+  }, [successCount, failCount, serverFailCount]);
+
+  const subscribeToTopic = (topic, callback) => {
     const subscription = client.current.subscribe(topic, (res) => {
-        const payload = JSON.parse(res.body);
-        console.log(`Received message for ${topic}:`, payload);
-        // 여기에서 payload를 처리
-        callback(payload);
-      });
-  }
+      const payload = JSON.parse(res.body);
+      // console.log(`Received message for ${topic}:`, payload);
+      // 여기에서 payload를 처리
+      callback(payload);
+    });
+  };
   const subscribeSuccess = (payload) => {
-    
-        // console.log(res)
-        setSuccessCount(payload);
-        // console.log(payload);
+    console.log("payload: ", payload);
+    setSuccessCount((prevSuccessCount) => [
+      ...prevSuccessCount,
+      { x: new Date(payload.date), y: payload.time },
+    ]);
+  };
 
-        // setErrorCount(json_body.slice());
-        // setranking (json_body.data.slice());
-        // console.log(ranking.current+"asdasdas")
-        // data.push(json_body);
-        // setLog(json_body);
-        // console.log(res.body);
+  const subscribeFail = (payload) => {
+    setFailCount((prevFailCount) => [
+      ...prevFailCount,
+      { x: new Date(payload.date), y: payload.time },
+    ]);
+  };
 
+  const subscribeServerFail = (payload) => {
+    setServerFailCount((prevServerFailCount) => [
+      ...prevServerFailCount,
+      { x: new Date(payload.date), y: payload.time },
+    ]);
+  };
+
+  const [series, setSeries] = useState([
+    {
+      name: "success",
+      data: successCount.slice(), // 배열 복사
+    },
+    {
+      name: "fail",
+      data: failCount.slice(), // 배열 복사
+    },
+    {
+      name: "serverFail",
+      data: serverFailCount.slice(), // 배열 복사
+    },
+  ]);
+
+  useEffect(() => {
+    //웹소켓
+
+    connect(); // 마운트시 실행
+    // const interval = setInterval(() => {
+    //   // getNewSeries();
+
+    //   ApexCharts.exec("realtime", "updateSeries", [
+    //     {
+    //       data: successCount,
+    //     },
+    //   ]);
+    // }, 1000);
+
+    return () => {
+      // clearInterval(interval);
+      disconnect();
     };
+  }, []);
 
-    const subscribeFail = () => {
-        client.current.subscribe('/sub/fail-time-check', (res) => { // server에게 메세지 받으면
-          // console.log(res)
-          const json_body = JSON.parse(res.body);
-          console.log(json_body);
-        //   setErrorCount(json_body.slice());
-          // setranking (json_body.data.slice());
-          // console.log(ranking.current+"asdasdas")
-    ;      // data.push(json_body);
-          // setLog(json_body);
-          // console.log(res.body);
-        });
-      };
-
-      const subscribeServerFail = () => {
-        client.current.subscribe('/sub/server-fail-time-check', (res) => { // server에게 메세지 받으면
-          // console.log(res)
-          const json_body = JSON.parse(res.body);
-          console.log(json_body);
-        //   setErrorCount(json_body.slice());
-          // setranking (json_body.data.slice());
-          // console.log(ranking.current+"asdasdas")
-    ;      // data.push(json_body);
-          // setLog(json_body);
-          // console.log(res.body);
-        });
-      };
-  
-    useEffect(() => {
-  //웹소켓 
-  
-   connect(); // 마운트시 실행
-      const interval = setInterval(() => {
-        // getNewSeries();
-  
-        
-      ApexCharts.exec('realtime', 'updateSeries', [
-          {
-            data: data
-          }
-        ]);
-      }, 1000);
-  
-      return () => {
-        clearInterval(interval);
-        disconnect();
-      };
-    }, []);
-
-    return (
-        <>
-        <Box bg='white' w='40vw' h='50vh' borderRadius="20px"  boxShadow="lg">
-        <Text fontWeight='Bold' p={5} >200&400&500 발생 횟수</Text>
-            <ReactApexChart options={options} series={series} type="line" height="80%" width="100%"/>
-        </Box>
-        </>
-    );
+  return (
+    <>
+      <Box bg="white" w="40vw" h="55vh" borderRadius="20px" boxShadow="lg">
+        <Text fontWeight="Bold" p={5}>
+          200 400 500 발생 횟수
+        </Text>
+        <ReactApexChart options={options} series={series} type="line" height="80%" width="100%" />
+      </Box>
+    </>
+  );
 }
 export default Chart6;
