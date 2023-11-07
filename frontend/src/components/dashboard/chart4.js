2fbac309b0962b884191a2defd9724e30c0a4849
cvivis <hsy0754@naver.com>
2023-11-07 15:54:25 +0900

    :sparkles: Feat: Add dashboard chart

--- frontend/src/components/dashboard/chart4.js
+++ frontend/src/components/dashboard/chart4.js
@@ -1,11 +1,76 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import { Box,Text } from "@chakra-ui/react";
+import * as StompJs from '@stomp/stompjs';
+import {Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,TableContainer,} from '@chakra-ui/react'
 function Chart4() {
+    const client = useRef({}); 
+    const [data,setData] = useState([]);
+    const connect = () =>{
+        client.current = new StompJs.Client({
+            brokerURL: 'ws://localhost:8080/ws/monitoring',
+            onConnect:() =>{
+               // Do something, all subscribes must be done is this callback
+              console.log("연결 SUB");
+                subscribe();
+            },
+        });
+        client.current.activate();
+    }
+  
+    const disconnect = () => {
+        client.current.deactivate(); // 활성화된 연결 끊기 
+    };
+  
+    const subscribe = () => {
+      client.current.subscribe('/sub/usage-ranking', (res) => { // server에게 메세지 받으면
+        // console.log(res)
+        const json_body = JSON.parse(res.body);
+        setData(json_body.data.slice());
+        // console.log(data.map((info,rank)=>info.url));
+        });
+    };
+
+    useEffect(() => {
+        connect();
+        // const interval = setInterval(() => {
+            
+        // }, 1000);
+    
+        return () => {
+    
+        //   clearInterval(interval);
+          disconnect();
+        };
+      }, []);
+
+  
 
     return (
         <>
-        <Box bg='white' w='36vw' h='40vh' borderRadius="20px"  boxShadow="lg">
-            <Text p={3}>CPU</Text>
+        <Box bg='white' w='40vw' h='55vh' borderRadius="20px"  boxShadow="lg">
+            <Text fontWeight='Bold' p={5} >API TOP5</Text>
+            <TableContainer>
+            <Table variant='simple' w="100%" h="100%" minH="50px">
+                <Thead>
+                <Tr>
+                    <Th>Ranking</Th>
+                    <Th>URL</Th>
+                    <Th>METHOD</Th>
+                    <Th isNumeric>USAGE</Th>
+                </Tr>
+                </Thead>
+                <Tbody>
+                    {data.map((info,rank)=>(
+                    <Tr key={rank}>
+                        <Td>{info.ranking}</Td>
+                        <Td>{info.url}</Td>
+                        <Td>{info.method}</Td>
+                        <Td isNumeric>{info.usage}</Td>
+                    </Tr>
+                    ))} 
+                </Tbody>
+            </Table>
+            </TableContainer>
         </Box>
         </>
     );
--- frontend/src/components/dashboard/chart5.js
+++ frontend/src/components/dashboard/chart5.js
@@ -1,11 +1,70 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from 'react';
 import { Box,Text } from "@chakra-ui/react";
+import * as StompJs from '@stomp/stompjs';
+import {Table,Thead,Tbody,Tfoot,Tr,Th,Td,TableCaption,TableContainer,} from '@chakra-ui/react'
 function Chart5() {
+    const client = useRef({}); 
+    const [data,setData] = useState([]);
+    const connect = () =>{
+        client.current = new StompJs.Client({
+            brokerURL: 'ws://localhost:8080/ws/monitoring',
+            onConnect:() =>{
+               // Do something, all subscribes must be done is this callback
+              console.log("연결 SUB");
+                subscribe();
+            },
+        });
+        client.current.activate();
+    }
+  
+    const disconnect = () => {
+        client.current.deactivate(); // 활성화된 연결 끊기 
+    };
+  
+    const subscribe = () => {
+      client.current.subscribe('/sub/error-count', (res) => { // server에게 메세지 받으면
+        const json_body = JSON.parse(res.body);
+        setData(json_body.slice());
+        console.log(data.map((info,rank)=>info.errorType));
+        });
+    };
+
+    useEffect(() => {
+        connect();
+        // const interval = setInterval(() => {
+            
+        // }, 1000);
+    
+        return () => {
+    
+        //   clearInterval(interval);
+          disconnect();
+        };
+      }, []);
 
     return (
         <>
-        <Box bg='white' w='36vw' h='40vh' borderRadius="20px"  boxShadow="lg">
-            <Text p={3}>CPU</Text>
+        <Box bg='white' w='36vw' h='55vh' borderRadius="20px"  boxShadow="lg">
+            <Text fontWeight='Bold' p={5} >Error Count</Text>
+            <TableContainer>
+            <Table variant='simple' w="100%" h="100%" minH="50px">
+                <Thead>
+                <Tr>
+                    <Th>Error Type</Th>
+                    <Th isNumeric>Count</Th>
+                </Tr>
+                </Thead>
+                <Tbody>
+                    {data.map((info,index)=>(
+                    <Tr key={index}>
+                        <Td>{info.errorType}</Td>
+                        <Td isNumeric>{info.count}</Td>
+                        {/* <Td>{info.detailInfo}</Td> */}
+                    </Tr>
+                    ))} 
+                </Tbody>
+            </Table>
+            </TableContainer>
         </Box>
         </>
     );
