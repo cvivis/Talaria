805804b5287162ff8e93a915194b9df365e5680e
Lee-Jiseung <ljss98@naver.com>
2023-11-07 16:37:29 +0900

    :sparkles: Feat: Implement ApisDto

---
+++ backend/src/main/java/com/hermes/talaria/domain/apis/dto/ApisDto.java
@@ -0,0 +1,42 @@
+package com.hermes.talaria.domain.apis.dto;
+
+import com.hermes.talaria.domain.apis.constant.ApisStatus;
+import com.hermes.talaria.domain.apis.constant.RawType;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class ApisDto {
+	private Long apisId;
+	private Long memberId;
+	private String name;
+	private String webServerUrl;
+	private String urlSuffix;
+	private String swaggerContent;
+	private ApisStatus status;
+	private Long quota;
+	private RawType rawType;
+	private String[] ips;
+
+	@Builder
+	public ApisDto(Long apisId, Long memberId, String name, String webServerUrl, String urlSuf1fix,
+		String swaggerContent, ApisStatus status, Long quota, RawType rawType, String[] ips) {
+		this.apisId = apisId;
+		this.memberId = memberId;
+		this.name = name;
+		this.webServerUrl = webServerUrl;
+		this.urlSuffix = urlSuffix;
+		this.swaggerContent = swaggerContent;
+		this.status = status;
+		this.quota = quota;
+		this.rawType = rawType;
+		this.ips = ips;
+	}
+}
