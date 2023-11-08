26fdaa58ead215da1cfff5d98c4ba7e1d057889b
오승기 <dhtmdrl2004@knou.ac.kr>
2023-11-08 09:19:07 +0900

    :sparkles: Feat: Implement redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,17 +5,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from "./theme/theme.js";
-import { Scrollbars } from 'react-custom-scrollbars-2';
+import { Provider } from 'react-redux';
+import store from './components/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+export const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider 
-    theme={theme}
-    position="relative"
-    // resetCSS={false}
-  >
-    <App />
-  </ChakraProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ChakraProvider 
+        theme={theme}
+        position="relative"
+        // resetCSS={false}
+      >
+        <App />
+      </ChakraProvider>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
