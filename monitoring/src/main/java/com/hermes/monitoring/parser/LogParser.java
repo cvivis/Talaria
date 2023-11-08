79eb9dcd2e731805d28224f4916e05bc65c193d6
이승민 <0283esm@naver.com>
2023-11-08 17:49:53 +0900

    :art: Refactor: fix duplicated regular expression

--- monitoring/src/main/java/com/hermes/monitoring/parser/LogParser.java
+++ monitoring/src/main/java/com/hermes/monitoring/parser/LogParser.java
@@ -15,8 +15,7 @@ import java.util.regex.Pattern;
 
 @Slf4j
 public class LogParser {
-    private static final String LOGFILE_REGEX = "^(\\S+) - - \\[([^\\]]+)\\] \"(\\S+) (\\S+) (\\S+)\" (\\d+) (\\d+) \"([^\"]*)\" \"([^\"]*)\" (\\d+\\.\\d+) (\\d+\\.\\d+)$";
-    private static final String LOGFILE_REGEX_NO_RESPONSE_TIME = "^(\\S+) - - \\[([^\\]]+)\\] \"(\\S+) (\\S+) (\\S+)\" (\\d+) (\\d+) \"([^\"]*)\" \"([^\"]*)\" (\\d+\\.\\d+) -$";
+    private static final String LOGFILE_REGEX = "^(\\S+) - - \\[([^\\]]+)\\] \"(\\S+) (\\S+) (\\S+)\" (\\d+) (\\d+) \"([^\"]*)\" \"([^\"]*)\" (\\d+\\.\\d+) ([\\d.\\-]+)$";
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss Z", Locale.US);
     public List<LogDto> parseLog(String filePath) throws IOException, ParseException {
         List<LogDto> logList = new ArrayList<>();
@@ -44,7 +43,7 @@ public class LogParser {
         String httpVersion = "httpVersion";
         String statusCode = "statusCode";
         double requestTime = 0;
-        double responseTime = 0;
+        double responseTime = -1;
         if (matcher.find()) {
             ipAddress = matcher.group(1);
             dateTime = sdf.parse(matcher.group(2));
@@ -75,9 +74,7 @@ public class LogParser {
     private LogDto parseLogEntry(String logEntry, long extractTime) throws ParseException {
         // 이 메서드에서 로그 항목을 파싱하여 LogDTO 객체로 변환합니다.
         Pattern pattern = Pattern.compile(LOGFILE_REGEX);
-        Pattern patternNoResponseTime = Pattern.compile(LOGFILE_REGEX_NO_RESPONSE_TIME);
         Matcher matcher = pattern.matcher(logEntry);
-        Matcher matcherNoResponseTime = patternNoResponseTime.matcher(logEntry);
         String ipAddress = "ip";
         Date dateTime = new Date();
         String httpMethod = "httpMethod";
@@ -94,31 +91,18 @@ public class LogParser {
             httpVersion = matcher.group(5);
             statusCode = matcher.group(6);
             requestTime = Double.parseDouble(matcher.group(10));
-            responseTime = Double.parseDouble(matcher.group(11));
+            if(!matcher.group(11).equals("-")) responseTime = Double.parseDouble(matcher.group(11));
             Date currentTime = new Date();
             Date externalTime = new Date(currentTime.getTime() - extractTime);
             if(dateTime.equals(currentTime) ||
                     dateTime.before(currentTime) &&
                             dateTime.after(externalTime)){
-                return new LogDto(ipAddress, dateTime, httpMethod, path, httpVersion, statusCode, requestTime, responseTime);
+                if(responseTime != -1){
+                    return new LogDto(ipAddress, dateTime, httpMethod, path, httpVersion, statusCode, requestTime, responseTime);
+                }else{
+                    return new LogDto(ipAddress, dateTime, httpMethod, path, httpVersion, statusCode, requestTime);
+                }
             }
-        } else if(matcherNoResponseTime.find()){
-            ipAddress = matcherNoResponseTime.group(1);
-            dateTime = sdf.parse(matcherNoResponseTime.group(2));
-            httpMethod = matcherNoResponseTime.group(3);
-            path = matcherNoResponseTime.group(4);
-            httpVersion = matcherNoResponseTime.group(5);
-            statusCode = matcherNoResponseTime.group(6);
-            requestTime = Double.parseDouble(matcherNoResponseTime.group(10));
-            Date currentTime = new Date();
-            Date externalTime = new Date(currentTime.getTime() - extractTime);
-            if(dateTime.equals(currentTime) ||
-                    dateTime.before(currentTime) &&
-                            dateTime.after(externalTime)){
-                return new LogDto(ipAddress, dateTime, httpMethod, path, httpVersion, statusCode, requestTime);
-            }
-        } else{
-            return null;
         }
         return null;
     }
