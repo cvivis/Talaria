34281397d1e28e88710b939ba56ef02b98a30aff
Lee-Jiseung <ljss98@naver.com>
2023-11-10 10:36:46 +0900

    :sparkles: Feat: Add annotations for enum type

--- backend/src/main/java/com/hermes/talaria/domain/apis/entity/Apis.java
+++ backend/src/main/java/com/hermes/talaria/domain/apis/entity/Apis.java
@@ -5,10 +5,14 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -32,6 +36,7 @@ import lombok.ToString;
 @EntityListeners(AuditingEntityListener.class)
 @ToString
 @TypeDef(name = "string-array", typeClass = StringArrayType.class)
+@DynamicInsert
 public class Apis implements Serializable {
 	@Id
 	@Column
@@ -50,13 +55,15 @@ public class Apis implements Serializable {
 	@Column
 	private String swaggerContent;
 
-	@Column
+	@Enumerated(EnumType.STRING)
+	@ColumnDefault("'PENDING'")
 	private ApisStatus status;
 
 	@Column
 	private Long quota;
 
-	@Column
+	@Enumerated(EnumType.STRING)
+	@ColumnDefault("'JSON'")
 	private RawType rawType;
 
 	@Type(type = "string-array")
@@ -65,7 +72,8 @@ public class Apis implements Serializable {
 
 	@Builder
 	public Apis(Long apisId, Long developerId, String name, String webServerUrl,
-		String swaggerContent, ApisStatus status, Long quota, RawType rawType, String[] whiteList) {
+		String swaggerContent, ApisStatus status, Long quota,
+		RawType rawType, String[] whiteList) {
 		this.apisId = apisId;
 		this.developerId = developerId;
 		this.name = name;
