92666f4a03a4316ef4428837f99491a4b356d21d
Lee-Jiseung <ljss98@naver.com>
2023-11-10 10:45:56 +0900

    :sparkles: Feat: Add status field to ApisRequest

--- backend/src/main/java/com/hermes/talaria/domain/apis/dto/ApisRequest.java
+++ backend/src/main/java/com/hermes/talaria/domain/apis/dto/ApisRequest.java
@@ -2,19 +2,24 @@ package com.hermes.talaria.domain.apis.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.hermes.talaria.domain.apis.constant.ApisStatus;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
 public class ApisRequest {
 	String name;
-	String url;
+	String webServerUrl;
+	ApisStatus status;
 
 	@Builder
-	public ApisRequest(String name, String url) {
+	public ApisRequest(String name, String webServerUrl, ApisStatus status) {
 		this.name = name;
-		this.url = url;
+		this.webServerUrl = webServerUrl;
+		this.status = status;
 	}
 }
