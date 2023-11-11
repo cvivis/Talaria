59f6c9d410536d966347e257b03a833fdca541f1
fluffymn <mineezm9@gmail.com>
2023-11-11 21:37:45 +0900

    ✨ Feat: Implement query method of key repository

--- backend/src/main/java/com/hermes/talaria/domain/key/repository/KeyRepository.java
+++ backend/src/main/java/com/hermes/talaria/domain/key/repository/KeyRepository.java
@@ -10,5 +10,7 @@ import com.hermes.talaria.domain.key.entity.Key;
 @Repository
 public interface KeyRepository extends JpaRepository<Key, Long> {
 	Optional<Key> findByKeyValue(String keyValue);
+
+	Optional<Key> findByKeyId(Long keyId);
 }
 
