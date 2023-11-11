a0daf2cc7319acbc0895a328f73f4d16034d3de5
fluffymn <mineezm9@gmail.com>
2023-11-11 21:24:51 +0900

    ✨ Feat: Add a field to apply subscription request

--- backend/src/main/java/com/hermes/talaria/domain/subscription/dto/ApplySubscriptionRequest.java
+++ backend/src/main/java/com/hermes/talaria/domain/subscription/dto/ApplySubscriptionRequest.java
@@ -2,25 +2,29 @@ package com.hermes.talaria.domain.subscription.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) // 무슨 용도인지 아직 모르겠다
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ApplySubscriptionRequest {
-    Long subscriptionId;
-    Long memberId;
-    Long apisId;
-    String content;
-    String address;
+	Long subscriptionId;
+	Long memberId;
+	Long apisId;
+	Long keyId;
+	String content;
+	String address;
 
-    @Builder
-    public ApplySubscriptionRequest(Long subscriptionId, Long memberId, Long apisId, String content, String address) {
-        this.subscriptionId = subscriptionId;
-        this.memberId = memberId;
-        this.apisId = apisId;
-        this.content = content;
-        this.address = address;
-    }
+	@Builder
+	public ApplySubscriptionRequest(Long subscriptionId, Long memberId, Long apisId, Long keyId, String content,
+		String address) {
+		this.subscriptionId = subscriptionId;
+		this.memberId = memberId;
+		this.apisId = apisId;
+		this.keyId = keyId;
+		this.content = content;
+		this.address = address;
+	}
 
 }
