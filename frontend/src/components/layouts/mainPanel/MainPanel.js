07fb3d7f141a7ba39fa768b870acf0c72b4a5654
오승기 <dhtmdrl2004@knou.ac.kr>
2023-11-13 14:09:47 +0900

    :art: Refactor: Delete wraping box

--- frontend/src/components/layouts/mainPanel/MainPanel.js
+++ frontend/src/components/layouts/mainPanel/MainPanel.js
@@ -3,10 +3,8 @@ import { Box } from '@chakra-ui/react';
 const MainPanel = ({setWidth, setHeight, children}) => {
 
     return (
-        <Box w={setWidth} h={setHeight} borderRadius='20px' mx='10'>
-            <Box>
-                {children}
-            </Box>
+        <Box w={setWidth} h={setHeight} minH="95vh" borderRadius='20px' mx='10'>
+            {children}
         </Box>
     );
 };
