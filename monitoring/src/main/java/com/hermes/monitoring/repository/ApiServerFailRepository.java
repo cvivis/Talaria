c902a4bb80914c5eacaded07f3343c6da289cb02
cvivis <hsy0754@naver.com>
2023-11-14 14:42:01 +0900

    :bug: Fix: if first usate in ranking update

--- monitoring/src/main/java/com/hermes/monitoring/repository/ApiServerFailRepository.java
+++ monitoring/src/main/java/com/hermes/monitoring/repository/ApiServerFailRepository.java
@@ -18,15 +18,14 @@ public interface ApiServerFailRepository extends JpaRepository<ServerFail, Integ
             "group by sf.date,sf.hour,sf.groupName order by sf.date asc, sf.hour desc")
     List<ApiHourlyCountDto> findDateAndCountByGroupName(@Param("groupName") String groupName);
 
-    @Query(value = "SELECT today.status_code as statusCode, today.count as count, round(cast(sum(hourly_count) as numeric) / (to_date(max(today),'YYYY-MM-DD') - cast(to_date(min(date),'YYYY-MM-DD') as date)),1) as avgCount" +
-            " from server_fail as a JOIN " +
-            "(select date as today, status_code, sum(hourly_count) as count "
-            + "from server_fail as b "
-            + "where b.group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date "
-            + "group by b.status_code, b.date "
-            + "order by count desc limit 3) as today " +
+    @Query(value = "select forCount.status_code as statusCode,forCount.count as count , COALESCE(avg, 0) as avg from (select date as today,status_code, sum(hourly_count) as count from server_fail where group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date group by status_code, date order by count desc limit 3) as forCount left join " +
+            "(SELECT today.status_code , round(cast(sum(hourly_count) as numeric)/ (to_date(max(today),'YYYY-MM-DD') - cast(to_date(min(date),'YYYY-MM-DD') as date)),1) as avg " +
+            "       from server_fail as a " +
+            "           JOIN " +
+            "(select date as today,status_code, sum(hourly_count) as count from server_fail where group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date group by status_code, date order by count desc limit 3) " +
+            "    as today " +
             "ON a.status_code = today.status_code and a.date != today.today " +
-            "where a.group_name = ?1 " +
-            "group by (today.status_code, today.count)" , nativeQuery = true)
+            "where group_name = ?1 " +
+            "group by (today.status_code, today.count) ) as forAvg on forCount.status_code = forAvg.status_code", nativeQuery = true)
     List<ApiRankingDto> findStatusCodeAndCountAndAvgCount(String groupName);
 }
--- monitoring/src/main/java/com/hermes/monitoring/repository/ClientFailGroupCountRepository.java
+++ monitoring/src/main/java/com/hermes/monitoring/repository/ClientFailGroupCountRepository.java
@@ -19,12 +19,15 @@ public interface ClientFailGroupCountRepository extends JpaRepository<ClientGrou
             "group by r.date,r.hour,r.groupName order by r.date asc , r.hour desc")
     List<ApiClientFailHourlyCountDto> findDateAndCountByUrlAndMethod(@Param("routing")String routingGroup);
 
-    @Query(value = "SELECT today.status_code as statusCode, today.count as count, round(cast(sum(hourly_count) as numeric) / (to_date(max(today),'YYYY-MM-DD') - cast(to_date(min(date),'YYYY-MM-DD') as date)),1) as avg" +
-            " from client_fail_group as a JOIN " +
-            "(select date as today, status_code, sum(hourly_count) as count from client_fail_group as b where b.group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date group by b.status_code, date order by count desc limit 3) as today " +
+    @Query(value = "select forCount.status_code as statusCode,forCount.count as count , COALESCE(avg, 0) as avg from (select date as today,status_code, sum(hourly_count) as count from client_fail_group where group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date group by status_code, date order by count desc limit 3) as forCount left join " +
+            "(SELECT today.status_code , round(cast(sum(hourly_count) as numeric)/ (to_date(max(today),'YYYY-MM-DD') - cast(to_date(min(date),'YYYY-MM-DD') as date)),1) as avg " +
+            "       from client_fail_group as a " +
+            "           JOIN " +
+            "(select date as today,status_code, sum(hourly_count) as count from client_fail_group where group_name = ?1 and to_date(date,'YYYY-MM-DD') = current_date group by status_code, date order by count desc limit 3) " +
+            "    as today " +
             "ON a.status_code = today.status_code and a.date != today.today " +
-            "where a.group_name = ?1 " +
-            "group by (today.status_code, today.count) order by count desc" , nativeQuery = true)
+            "where group_name = ?1 " +
+            "group by (today.status_code, today.count) ) as forAvg on forCount.status_code = forAvg.status_code" , nativeQuery = true)
     List<ClientFailGroupCountRepository.ApiClientFailRankingVo> findTodayAndAvgCount(String groupName);
 
     interface ApiClientFailRankingVo{
