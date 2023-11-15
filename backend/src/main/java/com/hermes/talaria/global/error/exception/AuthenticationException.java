39f5db4e83833342f9170873c2fa236e9ad5b67b
Yejining <kimyejin.kr@gmail.com>
2023-11-15 11:27:30 +0900

    :fire: Remove: backend files

--- backend/src/main/java/com/hermes/talaria/global/error/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.hermes.talaria.global.error.exception;
-
-import com.hermes.talaria.global.error.ErrorCode;
-
-public class AuthenticationException extends BusinessException{
-	public AuthenticationException(ErrorCode errorCode){
-		super(errorCode);
-	}
-}
