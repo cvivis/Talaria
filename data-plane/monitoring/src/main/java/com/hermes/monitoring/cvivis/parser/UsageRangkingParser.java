742c42b7da6db7e21584097f25354302cd404935
cvivis <hsy0754@naver.com>
2023-11-20 03:41:56 +0900

    :pencil2: Fix: change logs

--- data-plane/monitoring/src/main/java/com/hermes/monitoring/cvivis/parser/UsageRangkingParser.java
+++ data-plane/monitoring/src/main/java/com/hermes/monitoring/cvivis/parser/UsageRangkingParser.java
@@ -24,7 +24,6 @@ public class UsageRangkingParser {
         try{
             String line;
             while ((line = br.readLine()) != null) {
-//                log.info("line: "+line);
                 String urlMethod = parseLogEntry(line);
                 if(urlMethod != null){
                     map.put(urlMethod,map.getOrDefault(urlMethod,0)+1);
@@ -44,37 +43,72 @@ public class UsageRangkingParser {
         //log.info("keySetLIst : {}",keySet);
 
         List<UsageRankingDto> result = new ArrayList<>();
-        if(keySet.size() < 5){
-            while(keySet.size()<5){
-                keySet.add("UseAPI_Nothing");
-            }
-        }
+//        if(keySet.size() < 5){
+//            while(keySet.size()<5){
+//                keySet.add("UseAPI_Nothing");
+//            }
+//        }
+//        int ranking = 0;
+//        int beforeUsage = -1;
+//        int sameCount = 1;
+//        for(int i = 0; i < keySet.size();i++){
+//            String[] urlMethodArr = keySet.get(i).split("_");
+//            if(urlMethodArr[1].equals("Nothing")){
+//                UsageRankingDto usageRankingDto = UsageRankingDto.builder()
+//                        .url("")
+//                        .method("")
+//                        .usage(0)
+//                        .ranking(0)
+//                        .build();
+//                result.add(usageRankingDto);
+//                beforeUsage = 0;
+//            }else{
+//                int nowUsage = map.get(keySet.get(i));
+//                if(nowUsage == beforeUsage){
+//                    sameCount++;
+//                }
+//                else{
+//                    if(sameCount != 0){
+//                        ranking += sameCount;
+//                        sameCount = 1;
+//                    }
+//                    else{
+//                        ranking++;
+//                    }
+//                }
+//                if(ranking < 6){
+//                    UsageRankingDto usageRankingDto = UsageRankingDto.builder()
+//                            .url((urlMethodArr[0]))
+//                            .method(urlMethodArr[1])
+//                            .usage(nowUsage)
+//                            .ranking(ranking)
+//                            .build();
+//                    result.add(usageRankingDto);
+//                    beforeUsage = nowUsage;
+//                }
+//                else{
+//                    break;
+//                }
+//
+//            }
+//        }
+
         int ranking = 0;
         int beforeUsage = -1;
         int sameCount = 1;
         for(int i = 0; i < keySet.size();i++){
             String[] urlMethodArr = keySet.get(i).split("_");
-            if(urlMethodArr[1].equals("Nothing")){
-                UsageRankingDto usageRankingDto = UsageRankingDto.builder()
-                        .url("")
-                        .method("")
-                        .usage(0)
-                        .ranking(0)
-                        .build();
-                result.add(usageRankingDto);
-                beforeUsage = 0;
-            }else{
                 int nowUsage = map.get(keySet.get(i));
                 if(nowUsage == beforeUsage){
-                    sameCount++;
+                    sameCount++; // 같은 랭킨인 애들 수
                 }
                 else{
-                    if(sameCount != 0){
-                        ranking += sameCount;
-                        sameCount = 1;
+                    if(sameCount != 0){ // 동 순위인 애들 존재할때 다음 순위애가 동순위가 아님
+                        ranking += sameCount; //랭킹 증가
+                        sameCount = 1; // 다시 1부터
                     }
                     else{
-                        ranking++;
+                        ranking++; // 앞전과 useage 사용 횟수 다른데 동순위도 없음 Ranking++
                     }
                 }
                 if(ranking < 6){
@@ -92,7 +126,6 @@ public class UsageRangkingParser {
                 }
 
             }
-        }
         br.close();
         return result;
     }
